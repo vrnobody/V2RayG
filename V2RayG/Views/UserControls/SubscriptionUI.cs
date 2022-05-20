2670ffb2cfa3de0ec02e19abeeb44752946eaeb0
nobody <nobody@nowhere.mars>
2022-05-20 17:48:15 +0800

    Try to fix dispose exception in form option.

--- V2RayG/Views/UserControls/SubscriptionUI.cs
+++ V2RayG/Views/UserControls/SubscriptionUI.cs
@@ -64,10 +64,10 @@ namespace V2RayG.Views.UserControls
             var color = total > 0 ? Color.DarkGray : Color.Red;
             var text = $"{I18N.TotalNum}{total}";
             Apis.Misc.UI.Invoke(() =>
-           {
-               lbTotal.ForeColor = color;
-               lbTotal.Text = text;
-           });
+            {
+                lbTotal.ForeColor = color;
+                lbTotal.Text = text;
+            });
         }
 
         public bool IsEmpty() =>
