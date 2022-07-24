vrnobody/V2RayGCon

Revert "Disable individual logger."
This reverts commit 09755e5137934e8511a04d26b6bafd8395130e96.

V2RayGCon/Controllers/CoreServerComponent/Logger.cs
@@ -1,14 +1,10 @@
-﻿using System;
-
-namespace V2RayGCon.Controllers.CoreServerComponent
+﻿namespace V2RayGCon.Controllers.CoreServerComponent
 {
     sealed public class Logger :
         VgcApis.BaseClasses.ComponentOf<CoreServerCtrl>,
         VgcApis.Interfaces.CoreCtrlComponents.ILogger
     {
-        // VgcApis.Libs.Sys.QueueLogger qLogger = new VgcApis.Libs.Sys.QueueLogger();
-        public event EventHandler<string> OnLog;
-
+        VgcApis.Libs.Sys.QueueLogger qLogger = new VgcApis.Libs.Sys.QueueLogger();
         Services.Settings setting;
 
         public Logger(Services.Settings setting)
@@ -19,10 +15,10 @@ namespace V2RayGCon.Controllers.CoreServerComponent
         #region public methods
         public void Log(string message)
         {
+            qLogger.Log(message);
             try
             {
                 setting.SendLog($"[{coreInfo.GetIndex()}.{coreInfo.GetShortName()}] {message}");
-                OnLog?.Invoke(this, message);
             }
             catch { }
         }
@@ -44,7 +40,7 @@ namespace V2RayGCon.Controllers.CoreServerComponent
                 var title = coreInfo.GetTitle();
                 VgcApis.Misc.UI.Invoke(() =>
                 {
-                    form = Views.WinForms.FormSingleServerLog.CreateLogForm(title, this);
+                    form = Views.WinForms.FormSingleServerLog.CreateLogForm(title, qLogger);
                 });
             }
 
@@ -74,7 +70,7 @@ namespace V2RayGCon.Controllers.CoreServerComponent
         protected override void CleanupAfterChildrenDisposed()
         {
             VgcApis.Misc.UI.CloseFormIgnoreError(logForm);
-            // qLogger.Dispose();
+            qLogger.Dispose();
         }
         #endregion
     }

V2RayGCon/Views/WinForms/FormSingleServerLog.cs
@@ -6,9 +6,7 @@ namespace V2RayGCon.Views.WinForms
 {
     public partial class FormSingleServerLog : Form
     {
-        public static FormSingleServerLog CreateLogForm(
-            string title,
-            Controllers.CoreServerComponent.Logger logger)
+        public static FormSingleServerLog CreateLogForm(string title, VgcApis.Libs.Sys.QueueLogger logger)
         {
             FormSingleServerLog logForm = null;
             VgcApis.Misc.UI.Invoke(() =>
@@ -21,19 +19,15 @@ namespace V2RayGCon.Views.WinForms
 
         long updateTimestamp = -1;
         VgcApis.Libs.Tasks.Routine logUpdater;
-        VgcApis.Libs.Sys.QueueLogger qLogger = new VgcApis.Libs.Sys.QueueLogger();
-        Controllers.CoreServerComponent.Logger coreLogger;
+        VgcApis.Libs.Sys.QueueLogger qLogger;
 
         bool isPaused = false;
 
         FormSingleServerLog(
             string title,
-            Controllers.CoreServerComponent.Logger logger)
+            VgcApis.Libs.Sys.QueueLogger logger)
         {
-            coreLogger = logger;
-
-            coreLogger.OnLog += OnLogHandler;
-
+            this.qLogger = logger;
             logUpdater = new VgcApis.Libs.Tasks.Routine(
                 RefreshUi,
                 VgcApis.Models.Consts.Intervals.SiFormLogRefreshInterval);
@@ -41,11 +35,6 @@ namespace V2RayGCon.Views.WinForms
             InitializeComponent();
             VgcApis.Misc.UI.AutoSetFormIcon(this);
             this.Text = I18N.Log + " - " + title;
-        }
-
-        void OnLogHandler(object sender, string msg)
-        {
-            qLogger.Log(msg);
         }
 
         private void RefreshUi()
@@ -69,8 +58,9 @@ namespace V2RayGCon.Views.WinForms
         private void FormSingleServerLog_FormClosed(object sender, FormClosedEventArgs e)
         {
             logUpdater.Dispose();
-            coreLogger.OnLog -= OnLogHandler;
-            qLogger?.Dispose();
+
+            // Potential memory leaks
+            // qLogger.Dispose();
         }
 
         private void clearToolStripMenuItem_Click(object sender, EventArgs e)

vrnobody/V2RayG

V2RayG/Controllers/CoreServerComponent/Logger.cs
@@ -1,14 +1,10 @@
-﻿using System;
-
-namespace V2RayG.Controllers.CoreServerComponent
+﻿namespace V2RayG.Controllers.CoreServerComponent
 {
     sealed public class Logger :
         Apis.BaseClasses.ComponentOf<CoreServerCtrl>,
         Apis.Interfaces.CoreCtrlComponents.ILogger
     {
-        // Apis.Libs.Sys.QueueLogger qLogger = new Apis.Libs.Sys.QueueLogger();
-        public event EventHandler<string> OnLog;
-
+        Apis.Libs.Sys.QueueLogger qLogger = new Apis.Libs.Sys.QueueLogger();
         Services.Settings setting;
 
         public Logger(Services.Settings setting)
@@ -19,10 +15,10 @@ namespace V2RayG.Controllers.CoreServerComponent
         #region public methods
         public void Log(string message)
         {
+            qLogger.Log(message);
             try
             {
                 setting.SendLog($"[{coreInfo.GetIndex()}.{coreInfo.GetShortName()}] {message}");
-                OnLog?.Invoke(this, message);
             }
             catch { }
         }
@@ -44,7 +40,7 @@ namespace V2RayG.Controllers.CoreServerComponent
                 var title = coreInfo.GetTitle();
                 Apis.Misc.UI.Invoke(() =>
                 {
-                    form = Views.WinForms.FormSingleServerLog.CreateLogForm(title, this);
+                    form = Views.WinForms.FormSingleServerLog.CreateLogForm(title, qLogger);
                 });
             }
 
@@ -74,7 +70,7 @@ namespace V2RayG.Controllers.CoreServerComponent
         protected override void CleanupAfterChildrenDisposed()
         {
             Apis.Misc.UI.CloseFormIgnoreError(logForm);
-            // qLogger.Dispose();
+            qLogger.Dispose();
         }
         #endregion
     }

V2RayG/Views/WinForms/FormSingleServerLog.cs
@@ -6,9 +6,7 @@ namespace V2RayG.Views.WinForms
 {
     public partial class FormSingleServerLog : Form
     {
-        public static FormSingleServerLog CreateLogForm(
-            string title,
-            Controllers.CoreServerComponent.Logger logger)
+        public static FormSingleServerLog CreateLogForm(string title, Apis.Libs.Sys.QueueLogger logger)
         {
             FormSingleServerLog logForm = null;
             Apis.Misc.UI.Invoke(() =>
@@ -21,19 +19,15 @@ namespace V2RayG.Views.WinForms
 
         long updateTimestamp = -1;
         Apis.Libs.Tasks.Routine logUpdater;
-        Apis.Libs.Sys.QueueLogger qLogger = new Apis.Libs.Sys.QueueLogger();
-        Controllers.CoreServerComponent.Logger coreLogger;
+        Apis.Libs.Sys.QueueLogger qLogger;
 
         bool isPaused = false;
 
         FormSingleServerLog(
             string title,
-            Controllers.CoreServerComponent.Logger logger)
+            Apis.Libs.Sys.QueueLogger logger)
         {
-            coreLogger = logger;
-
-            coreLogger.OnLog += OnLogHandler;
-
+            this.qLogger = logger;
             logUpdater = new Apis.Libs.Tasks.Routine(
                 RefreshUi,
                 Apis.Models.Consts.Intervals.SiFormLogRefreshInterval);
@@ -41,11 +35,6 @@ namespace V2RayG.Views.WinForms
             InitializeComponent();
             Apis.Misc.UI.AutoSetFormIcon(this);
             this.Text = I18N.Log + " - " + title;
-        }
-
-        void OnLogHandler(object sender, string msg)
-        {
-            qLogger.Log(msg);
         }
 
         private void RefreshUi()
@@ -69,8 +58,9 @@ namespace V2RayG.Views.WinForms
         private void FormSingleServerLog_FormClosed(object sender, FormClosedEventArgs e)
         {
             logUpdater.Dispose();
-            coreLogger.OnLog -= OnLogHandler;
-            qLogger?.Dispose();
+
+            // Potential memory leaks
+            // qLogger.Dispose();
         }
 
         private void clearToolStripMenuItem_Click(object sender, EventArgs e)